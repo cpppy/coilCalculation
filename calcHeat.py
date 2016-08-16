0c4f4478200f3e50f65a59b664bacb1a46dbe8df
cpppy <1107706059@qq.com>
2016-08-16 10:20:40 +0800

    Update calcHeat.py

--- calcHeat.py
+++ calcHeat.py
@@ -9,7 +9,6 @@ def electricCurrent(time):
     I = I0*exp(-Rd*time/L)
     return I
 
-
 def dIdt(time):
     I0 = 14000  
     R =  0.286
@@ -26,34 +25,7 @@ def Bvalue(time):
     B = 0.5*u0*I*(104/0.482 + 114/0.514 + 228/0.604 +720/0.7255)
     return B
     
-def dmagneticFieldStrenth(dIdt):    #计算磁场强度变化率（随时间）
+def dmagneticFieldStrenth(dIdt):    #璁＄畻纾佸満寮哄害鍙樺寲鐜囷紙闅忔椂闂达級
     u0 = 4*pi*10**(-7)
     dBdt = 0.5*u0*dIdt*(104/0.482 + 114/0.514 + 228/0.604 +720/0.7255)
     return dBdt
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
